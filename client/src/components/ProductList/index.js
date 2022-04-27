840b4b32758d5592135cc5b11581355ca5430223
Lindsey Dubnicka <lindseymiller2567@gmail.com>
2022-04-26 20:41:28 -0500

    added cart functionality

--- client/src/components/ProductList/index.js
+++ client/src/components/ProductList/index.js
@@ -1,10 +1,9 @@
 import React, { useEffect } from 'react';
 import { useQuery } from '@apollo/client';
 
+import ProductItem from '../ProductItem';
 import { useStoreContext } from '../../utils/GlobalState';
 import { UPDATE_PRODUCTS } from '../../utils/actions';
-
-import ProductItem from '../ProductItem';
 import { QUERY_PRODUCTS } from '../../utils/queries';
 import spinner from '../../assets/spinner.gif';
 
