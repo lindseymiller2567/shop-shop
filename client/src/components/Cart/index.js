c0ba4deaac58d61e50fcd5fc5964a09cce9a2513
Lindsey Dubnicka <lindseymiller2567@gmail.com>
2022-04-28 22:23:06 -0500

    created success componenet to save order in DB

--- client/src/components/Cart/index.js
+++ client/src/components/Cart/index.js
@@ -5,10 +5,17 @@ import { useStoreContext } from "../../utils/GlobalState";
 import { TOGGLE_CART, ADD_MULTIPLE_TO_CART } from "../../utils/actions";
 import "./style.css";
 import { idbPromise } from "../../utils/helpers";
+import { QUERY_CHECKOUT } from "../../utils/queries";
+import { loadStripe } from '@stripe/stripe-js';
+import { useLazyQuery } from "@apollo/client";
+
+const stripePromise = loadStripe('pk_test_TYooMQauvdEDq54NiTphI7jx');
 
 const Cart = () => {
 
   const [state, dispatch] = useStoreContext();
+
+  const [getCheckout, { data }] = useLazyQuery(QUERY_CHECKOUT);
 
   useEffect(() => {
     async function getCart() {
@@ -32,6 +39,28 @@ const Cart = () => {
     });
     return sum.toFixed(2);
   }
+
+  function submitCheckout() {
+    const productIds = [];
+
+    state.cart.forEach((item) => {
+      for (let i = 0; i < item.purchaseQuantity; i++) {
+        productIds.push(item._id);
+      }
+    });
+
+    getCheckout({
+      variables: { products: productIds }
+    });
+  }
+
+  useEffect(() => {
+    if (data) {
+      stripePromise.then((res) => {
+        res.redirectToCheckout({ sessionId: data.checkout.session });
+      });
+    }
+  }, [data]);
 
   if (!state.cartOpen) {
     return (
@@ -60,7 +89,7 @@ const Cart = () => {
 
             {
               Auth.loggedIn() ?
-                <button>
+                <button onClick={submitCheckout}>
                   Checkout
                 </button>
                 :
